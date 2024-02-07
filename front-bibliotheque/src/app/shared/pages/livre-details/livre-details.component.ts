4963eab7b4c500f221dc326c080477ed5328338f
Anthonycbrl <94748779+Anthonycbrl@users.noreply.github.com>
2024-02-07 13:34:05 +0100

    added getId on details

--- front-bibliotheque/src/app/shared/pages/livre-details/livre-details.component.ts
+++ front-bibliotheque/src/app/shared/pages/livre-details/livre-details.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-livre-details',
   templateUrl: './livre-details.component.html',
-  styleUrl: './livre-details.component.css'
+  styleUrls: ['./livre-details.component.css']
 })
 export class LivreDetailsComponent {
-
+  id: string = '';
+  constructor(private route: ActivatedRoute) { }
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params['id']
+      this.loadDataById(this.id);
+    });
+  }
+  loadDataById(id: string) {
+    // charger les données en fonction de l'ID
+  }
 }
